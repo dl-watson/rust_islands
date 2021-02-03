16ae35dd92135f740bc1ee1f40378ce3d339cd87
dl-watson <dlarkinwatson@gmail.com>
2021-02-02 18:58:28 -0800

    build out union

--- src/disjoint.rs
+++ src/disjoint.rs
@@ -1,3 +1,5 @@
+use std::cmp::{max, min};
+
 // Define a new data structure, with one (unnamed) field
 // i32 is the default you should pick for a number, unless you have an argument for using a different type of number
 #[derive(Debug)]
@@ -7,8 +9,8 @@ impl DisjointSet {
     pub fn new(size: usize) -> DisjointSet {
         // Create a vector (dynamic array), passing it the initial value and its size
         // None is the Rust equivalent of null
-        let v = vec![None;size];
-        DisjointSet(v)
+        let constructor = vec![None;size];
+        DisjointSet(constructor)
     }
 
     // find is a method on a DisjointSet; it borrows the DisjointSet so that it can look at its contents
@@ -23,23 +25,104 @@ impl DisjointSet {
 
     // write borrow
     pub fn union(&mut self, key1: i32, key2: i32) {
-        let root = self.find(key1);
-        self.0[key2 as usize] = Some(root);
-    }
+        let root1 = self.find(key1);
+        let root2 = self.find(key2);
 
+        // self.0[key2 as usize] = Some(root);
+
+        let root1_is_identifier = root1 == key1;  
+        let root2_is_identifier = root2 == key2;  
+
+        // Three cases for union
+        // Case 1: both of our numbers are their own identifiers
+        if root1_is_identifier && root2_is_identifier {
+            // set the root of the larger to be the root of the smaller 
+            if root1 < root2 {
+                self.0[root2 as usize] = Some(root1);
+            } else {
+                self.0[root1 as usize] = Some(root2);
+            }
+        }
+
+        // Case 2: both aren't
+        else if !root1_is_identifier && !root2_is_identifier {
+            /*
+            [None, Some(0), None, Some(2)]
+            union(1, 3)
+            -> [None, Some(0), Some(0), Some(0)]
+            */
+            // the winning identifier will be whichever one is smaller
+            // the losing identifier now points to the winning 
+            // all the references to the losing identifier will now point to the winning
+            let winner = min(root1, root2);
+            let loser = max(root1, root2);
+
+            self.0[loser as usize] = Some(winner);
+            // for option (nullable i32) in the data structure
+            // mutably borrowed
+            for opt in &mut self.0 {
+                // match is a switch on steroids
+                match opt {
+                    // we only care about looking at references
+                    // every non-null value is a reference in our data structure
+                    Some(x) => {
+                        // only set references to the losing value
+                        // this x is an &mut i32 (& means reference, &mut means mutable reference)
+                        if *x == loser {
+                            // point them to the winning value
+                            // at the location of x, set the value to winner
+                            *x = winner
+                        }
+                    },
+                    // if it's null, we don't care
+                    _ => {}
+                }
+            }
+        }
+        
+        // Case 3: one isn't an identifier
+        else {
+            let (identifier, not) = if root1_is_identifier {
+                (root1, root2)
+            } else {
+                (root2, root1)
+            };
+
+            // set the one that isn't to the one that is
+            self.0[identifier as usize] = Some(not);
+        }
+    }
 }
 
+pub struct SafeDisjointSet(DisjointSet);
+
+impl SafeDisjointSet {
+    pub fn new(size: usize) -> Self {
+        Self(DisjointSet::new(size))
+    }
+    
+    pub fn find(&self, key: i32) -> Option<i32> {
+        if key as usize >= self.0.0.len() {
+            None
+        } else {
+            Some(self.0.find(key))
+        }
+    }
+
+    // pub fn union(&mut self, key1: i32, key2: i32) {
+
+    // }
+}
 /* 
 Known bugs:
 
 Trying to find a key value that's out of the vector's size scope will cause the program to CRASH
-
-If everything is pointing to three, three will be None and everything else will be three
-With union, you need to recursively find the root/identifier for the set ~ or you know what I mean
 */ 
 
 #[cfg(test)]
 mod tests {
+    use std::vec;
+
     use super::DisjointSet;
 
     #[test]
@@ -54,5 +137,33 @@ mod tests {
         assert_ne!(ds.find(1), 0);
         ds.union(0, 1);
         assert_eq!(ds.find(1), 0);
+    }
+
+    #[test]
+    fn num_are_own_identifier() {
+        let mut ds = DisjointSet::new(5);
+        ds.union(0, 1);
+        ds.union(4, 3);
+        assert_eq!(ds.0, vec![None, Some(0), None, None, Some(3)]);
+    }
+
+    #[test]
+    fn neither_are_identifier() {
+        let mut ds = DisjointSet::new(5);
+        ds.union(0,1 );
+        ds.union(3, 4);
+        assert_eq!(ds.0, vec![None, Some(0), None, None, Some(3)]);
+        ds.union(1, 4);
+        assert_eq!(ds.0, vec![None, Some(0), None, Some(0), Some(0)]);
+    }
+
+    #[test]
+    fn one_isnt_an_identifier() {
+        let mut ds = DisjointSet::new(5);
+        ds.union(0,1 );
+        ds.union(3, 4);
+        assert_eq!(ds.0, vec![None, Some(0), None, None, Some(3)]);
+        ds.union(1, 3);
+        assert_eq!(ds.0, vec![None, Some(0), None, Some(0), Some(3)]);
     }
 }
